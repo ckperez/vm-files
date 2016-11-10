c7ddaf4093360834167e3c28e5f92f04ca86daf0
Chris Perez <chris.k.perez@gmail.com>
2016-11-10 08:09:27 -0800

    shit

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express();
 var port = process.env.port || 3000;
 
+var getFiles = function(folder){ //will return array of items in folder
+	fs.readdir(folder, (err, data)=>{
+		if (err){
+			throw err;
+		}
+		return data;
+	});
+};
+
 app.use(express.static(__dirname + '/build'));
-/*
-app.get('*', function(request, response) {
- console.log('New request:', request.url);
- response.sendFile('index.html', { root: '.' });
-}); 
-*/
+
 app.listen(port, function(){
 	console.log(`server up on ${port}`);
 });
